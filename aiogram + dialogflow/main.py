6627ca51664b6682501092abc8ce51d4697b6637
jezvgg <jezv@yandex.ru>
2023-06-23 23:12:30 +0800

    add telegram bot DialogFlow

---
+++ aiogram + dialogflow/main.py
@@ -0,0 +1,30 @@
+from aiogram import Bot, Dispatcher, executor
+from google.cloud import dialogflow
+from pathlib import Path
+import json
+
+settings_file = open(Path('settings.json')) # Загружаем json файл с настройками
+settings = json.load(settings_file)
+
+bot = Bot(token=settings['bot-token'])
+dp = Dispatcher(bot)
+
+project_id = settings['project_id'] # Записываем настройки
+session_id = settings['session_id']
+language = settings['language']
+
+session_client = dialogflow.SessionsClient()                # Создание сессии
+session = session_client.session_path(project_id, session_id)
+
+
+@dp.message_handler()
+def start(message):
+    text = message.text
+
+    text_input = dialogflow.TextInput(text=text, language_code=language) # Ввод текста
+    query_input = dialogflow.QueryInput(text=text_input) # Запрос к агенту по тексту
+    response = session_client.detect_intent(
+    request={"session": session, "query_input": query_input}
+    )
+
+    message.answer(response.query_result.fulfillment_text)
